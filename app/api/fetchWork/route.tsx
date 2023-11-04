02d636b3ee50397636b8df62b352e1e2f507bffd
John4E656F <jm49andres@gmail.com>
2023-11-04 12:11:19 +0100

    Remove unecessary Files

--- app/api/fetchWork/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { client } from '@/lib/api';
-
-export async function POST(req: NextRequest, res: NextResponse) {
-    try {
-        const contentType = await client.getContentType('portfolio');
-    }
-}
