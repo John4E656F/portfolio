8b9b97b0fe2f0395a90e115dd7fef8346605b1af
John4E656F <jm49andres@gmail.com>
2023-11-04 16:35:40 +0100

    Building case study page

--- app/casestudy/[slug]/page.tsx
+++ app/casestudy/[slug]/page.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 const CaseStudy = () => {
   return (
     <section className='w-full flex flex-col items-center select-none'>
-      <div className='container'>Hello World</div>
+      <div className='container flex flex-col py-5 gap-10'>
+        <div className='flex flex-col md:flex-row bg-red-200'>
+          <div className=''>
+            <h1>Yes Job</h1>
+            <h2>Horeca Hiring Made Easy: A Case Study</h2>
+          </div>
+          <img src='/yesjob.JPG' className='rounded w-full max-w-2xl' />
+        </div>
+        <div className='h-screen bg-blue-200'>Hello World</div>
+      </div>
     </section>
   );
 };
