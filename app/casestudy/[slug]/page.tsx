e1815b596af692fc2ee227e76598d7a0895af4c7
John4E656F <jm49andres@gmail.com>
2023-11-04 16:26:18 +0100

    Init

---
+++ app/casestudy/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CaseStudy = () => {
+  return (
+    <section className='w-full flex flex-col items-center select-none'>
+      <div className='container'>Hello World</div>
+    </section>
+  );
+};
+
+export default CaseStudy;
