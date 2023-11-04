13ca0a48755d8e8f633ce79733d9289034c48af5
John4E656F <jm49andres@gmail.com>
2023-11-04 12:31:54 +0100

    Revert "Successfully implimented contentful"
    This reverts commit 24d6a52fa01255df96326639a84a077abb1f69a3.

--- components/Works/Works.tsx
+++ components/Works/Works.tsx
@@ -1,58 +1,13 @@
-import { useState, useEffect } from 'react';
+import { RefObject } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { Work } from '..';
 import type { RefProps } from '@/types';
-import { Entry } from 'contentful';
-import { client } from '@/lib/api';
-import { TypePortfolio } from '@/types';
-import { getAllWorkList } from '@/lib/api';
-
-interface WorkData {
-  title: string;
-  subTitle: string;
-  featuredImage: string;
-  githubLink: string;
-  websiteLink: string;
-  right: boolean;
-}
-
-async function getWorkData() {
-  const res = await getAllWorkList();
-  if (!res) {
-    throw new Error('Failed to fetch data');
-  }
-  return res;
-}
+import workData from '@/data/works.json';
 
 export const Works: React.FC<RefProps> = ({ parentRef }) => {
-  const [workData, setWorkData] = useState<TypePortfolio[]>([]);
   const { scrollYProgress } = useScroll({ target: parentRef });
   const bgColor = useTransform(scrollYProgress, [0, 0.5], ['#ffffff', '#000000']);
-
-  useEffect(() => {
-    // Fetch data from Contentful here
-    async function fetchData() {
-      try {
-        const response = await getAllWorkList();
-
-        const mappedData: TypePortfolio[] = response.map((item: TypePortfolio<any>) => ({
-          title: item.fields.title,
-          subTitle: item.fields.subTitle,
-          featuredImage: item.fields.featuredImage?.fields.file.url,
-          githubLink: item.fields.githubLink,
-          websiteLink: item.fields.websiteLink,
-          right: item.fields.right,
-        }));
-        console.log(mappedData);
-
-        setWorkData(mappedData);
-      } catch (error) {
-        console.error('Error fetching data from Contentful:', error);
-      }
-    }
-
-    fetchData();
-  }, []);
+  console.log(workData);
 
   return (
     <motion.section
@@ -71,9 +26,9 @@ export const Works: React.FC<RefProps> = ({ parentRef }) => {
             parentRef={parentRef}
             title={workData.title}
             subTitle={workData.subTitle}
-            image={workData.featuredImage}
-            website={workData.websiteLink}
-            github={workData.githubLink}
+            image={workData.image}
+            website={workData.website}
+            github={workData.github}
             right={workData.right}
           />
         ))}
--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,1 @@
 export type { RefProps } from './RefProps';
-export type { TypeCaseStudy, TypeCaseStudyFields } from './TypeCaseStudy';
-export type { TypePortfolio, TypePortfolioFields } from './TypePortfolio';
